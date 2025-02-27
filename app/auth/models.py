c5832d717b90383a68b582ec3f2e60933dc2b915
n1x9s <spl1ntero@yandex.ru>
2025-02-27 14:43:58 +0300

    Упрощение модели пользователя и удаление миграций

--- app/auth/models.py
+++ app/auth/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from app.dao.database import Base
 
@@ -9,32 +9,8 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
     name = Column(String, nullable=False)
-    role = Column(String, nullable=False)
-    created_at = Column(DateTime, nullable=False)
-    updated_at = Column(DateTime, nullable=False)
-    direction_id = Column(Integer, ForeignKey('directions.id'), nullable=False)
-    language_id = Column(Integer, ForeignKey('languages.id'), nullable=False)
-
-    direction = relationship("Direction", back_populates="users")
-    language = relationship("Language", back_populates="users")
 
     def __repr__(self):
         return f"{self.__class__.__name__}(id={self.id}, email={self.email})"
     
-
-class Direction(Base):
-    __tablename__ = 'directions'
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    users = relationship("User", back_populates="direction")
-
-
-
-class Language(Base):
-    __tablename__ = 'languages'
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    users = relationship("User", back_populates="language")
 
